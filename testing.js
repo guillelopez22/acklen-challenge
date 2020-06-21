1e953ccd4b76a56e6da88c060061e8c8ffde27fb
guillelopez22 <guillelopez_13@hotmail.com>
2020-06-21 15:59:39 -0600

    Retrieving select option values

--- testing.js
+++ testing.js
@@ -1,22 +1,35 @@
 // declarations
-const { Builder, By, Key, util} = require('selenium-webdriver');
+const { Builder, By, Key, util } = require('selenium-webdriver');
+const assert = require('assert');
+const AssertionError = require('assert').AssertionError;
 
-async function testConnection() {
+async function parkingCalculatorTests() {
     // define browser driver
     let driver = await new Builder().forBrowser('firefox').build();
     await driver.get('http://www.shino.de/parkcalc/');
-    await driver.getTitle().then(function(title) {
+    await driver.getTitle().then(function (title) {
         console.log(title);
         console.log('Successful connection to site');
     })
 
-    let parkingLot = await driver.findElement(By.css('#ParkingLot>option:nth-child(1)')).getAttribute('value').then(function(value) {
-        console.log(value);
-        
+    // getting select options
+    let options = [];
+    await driver.findElement(By.name('ParkingLot')).then(async function (element) {
+        await element.findElements(By.tagName('option')).then(async function (optionArray) {
+            for (let index = 0; index < optionArray.length; index++) {
+                await optionArray[index].getAttribute('value').then(function(value) {
+                    options.push(value)
+                })
+            }
+        })
     })
+    console.log(options);
+    
+
     driver.quit();
 }
 
 
 
-testConnection()
+
+parkingCalculatorTests()
