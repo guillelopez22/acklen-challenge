06679f8a5e8f5da69b146e7f7afcdd12deedba26
guillelopez22 <guillelopez_13@hotmail.com>
2020-06-21 19:07:41 -0600

    adding some mocha

--- testing.js
+++ testing.js
@@ -7,32 +7,35 @@ async function parkingCalculatorTests() {
     // define browser driver
     let driver = await new Builder().forBrowser('firefox').build();
     await driver.get('http://www.shino.de/parkcalc/');
-    await driver.getTitle().then(function (title) {
-        console.log(title);
-        console.log('Successful connection to site');
-    })
-
-    // getting select options
-    let options = [];
-    await driver.findElement(By.name('ParkingLot')).then(async function (element) {
-        await element.findElements(By.tagName('option')).then(async function (optionArray) {
-            for (let index = 0; index < optionArray.length; index++) {
-                await optionArray[index].getAttribute('value').then(function(value) {
-                    options.push(value)
-                });
-            }
+    
+    describe('Parking Calculator Tests', async function() {
+        describe('driver.get()', async function() {
+            it('No error when connecting', async function() {
+                this.timeout(5000);
+            });
         });
     });
 
+    // getting select options
+    // let options = [];
+    // await driver.findElement(By.id('ParkingLot')).then(async function (element) {
+    //     await element.findElements(By.tagName('option')).then(async function (optionArray) {
+    //         for (let index = 0; index < optionArray.length; index++) {
+    //             await optionArray[index].getAttribute('value').then(function(value) {
+    //                 options.push(value)
+    //             });
+    //         }
+    //     });
+    // });
+
+    
+    // // valet parking 
+    // await driver.findElement(By.id('StartingDate')).click();
+    // await driver.findElement(By.id('StartingDate')).clear();
+    // await driver.findElement(By.id('StartingDate')).sendKeys('20/12/2020')
+
     
 
-
-
-
-
-    
-
-    driver.quit();
 }
 
 
